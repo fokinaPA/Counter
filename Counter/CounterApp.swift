c1a26729ce201bb132602c6296ed725a2d0cfe21
fokinaPA <fokina.p.a@gmail.com>
2023-02-21 14:51:12 +0000

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Polina on 21/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
